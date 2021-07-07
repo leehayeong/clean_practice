e0607576d9d7f2a25df573b3a19ce683eaae5adc
Lee Hayeong <lhaayyy@gmail.com>
2021-07-07 17:05:51 +0900

    [#1] init commit

---
+++ app/src/main/java/com/example/cleanexample/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cleanexample.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cleanexample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
